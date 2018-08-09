0c8779f6a0f6ff21a017d36cdd374dcd2211cd89
svetla9101 <svetla.yukhnovich@gmail.com>
2018-08-09 15:20:37 +0300

    Added a Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def inform
+  end
+
+  def important
+  end
+
+  def use
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/contact'
+  get 'pages/about'
+  get 'pages/inform'
+  get 'pages/important'
+  get 'pages/use'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
